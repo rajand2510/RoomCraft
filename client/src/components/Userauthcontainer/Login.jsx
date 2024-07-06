b128392d67af318de71277a8c388b2dc1cb00972
Rajan Dhariyaparmar <rajandalvadi2510@gmail.com>
2024-07-06 09:15:35 +0530

    log in

--- client/src/components/Userauthcontainer/Login.jsx
+++ client/src/components/Userauthcontainer/Login.jsx
@@ -94,7 +94,13 @@ function LogInForm() {
       </button>
       <div className="flex gap-2 self-start mt-4 text-sm text-green-800">
         <p className="grow">Don&apos;t have an account</p>
-        <a href="/signup" className="font-bold">Sign up</a>
+        <a href="#" 
+        className="font-bold"
+        onClick={(e) => {
+          e.preventDefault();
+          navigate('/signup');
+        }}
+        >Sign up</a>
       </div>
     </form>
   );
