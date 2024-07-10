ff0b433376badae9276d021f523745e3bbbcc2df
Rajan Dhariyaparmar <rajandalvadi2510@gmail.com>
2024-07-10 17:48:07 +0530

    model interaction

--- client/src/components/xr-hit-model/XrHitModelConatainer.jsx
+++ client/src/components/xr-hit-model/XrHitModelConatainer.jsx
@@ -1,91 +1,3 @@
-import React, { useRef, useState } from 'react';
-import { useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from "@react-three/drei";
-import { useThree } from "@react-three/fiber";
-import { Interactive, useHitTest, useXR } from "@react-three/xr";
-import { Canvas } from "@react-three/fiber";
-import { ARButton, XR } from "@react-three/xr";
-import { useLocation } from 'react-router-dom';
-
-const Model = ({ gltfPath, position }) => {
-  const gltf = useLoader(GLTFLoader, gltfPath);
-  return <primitive position={position} object={gltf.scene} />;
-};
-
-const XrHitModel = ({ gltfPath }) => {
-  const reticleRef = useRef();
-  const [models, setModels] = useState([]);
-
-  const { isPresenting } = useXR();
-
-  useThree(({ camera }) => {
-    if (!isPresenting) {
-      camera.position.z = 3;
-    }
-  });
-
-  useHitTest((hitMatrix, hit) => {
-    hitMatrix.decompose(
-      reticleRef.current.position,
-      reticleRef.current.quaternion,
-      reticleRef.current.scale
-    );
-
-    reticleRef.current.rotation.set(-Math.PI / 2, 0, 0);
-  });
-
-  const placeModel = (e) => {
-    let position = e.intersection.object.position.clone();
-    let id = Date.now();
-    setModels([{ position, id }]);
-  };
-
-  return (
-    <>
-      <OrbitControls />
-      <ambientLight />
-      {isPresenting &&
-        models.map(({ position, id }) => {
-          return <Model key={id} gltfPath={gltfPath} position={position} />;
-        })}
-      {isPresenting && (
-        <Interactive onSelect={placeModel}>
-          <mesh ref={reticleRef} rotation-x={-Math.PI / 2}>
-            <ringGeometry args={[0.1, 0.25, 32]} />
-            <meshStandardMaterial color={"white"} />
-          </mesh>
-        </Interactive>
-      )}
-
-      {!isPresenting && <Model gltfPath={gltfPath} />}
-    </>
-  );
-};
-
-const XrHitModelContainer = () => {
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const gltfPath = params.get('gltfPath') || './models/default.gltf';
-
-  return (
-    <>
-      <ARButton sessionInit={{
-        requiredFeatures: ["hit-test"]
-      }} />
-      <Canvas>
-        <XR>
-          <XrHitModel gltfPath={gltfPath} />
-        </XR>
-      </Canvas>
-    </>
-  );
-};
-
-export default XrHitModelContainer;
-
-
-
 // import React, { useRef, useState } from 'react';
 // import { useLoader } from '@react-three/fiber';
 // import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
@@ -95,17 +7,16 @@ export default XrHitModelContainer;
 // import { Canvas } from "@react-three/fiber";
 // import { ARButton, XR } from "@react-three/xr";
 // import { useLocation } from 'react-router-dom';
-// import * as THREE from 'three';
 
-// const Model = ({ gltfPath, position, scale }) => {
+// const Model = ({ gltfPath, position }) => {
 //   const gltf = useLoader(GLTFLoader, gltfPath);
-//   return <primitive position={position} scale={scale} object={gltf.scene} />;
+//   return <primitive position={position} object={gltf.scene} />;
 // };
 
 // const XrHitModel = ({ gltfPath }) => {
 //   const reticleRef = useRef();
 //   const [models, setModels] = useState([]);
-//   const [scale, setScale] = useState(new THREE.Vector3(1, 1, 1));
+
 //   const { isPresenting } = useXR();
 
 //   useThree(({ camera }) => {
@@ -130,34 +41,13 @@ export default XrHitModelContainer;
 //     setModels([{ position, id }]);
 //   };
 
-//   const handlePinch = (e) => {
-//     if (e.touches.length === 2) {
-//       const touch1 = e.touches[0];
-//       const touch2 = e.touches[1];
-
-//       const distance = Math.sqrt(
-//         (touch1.clientX - touch2.clientX) ** 2 +
-//         (touch1.clientY - touch2.clientY) ** 2
-//       );
-
-//       if (!handlePinch.prevDistance) {
-//         handlePinch.prevDistance = distance;
-//       }
-
-//       const scaleFactor = distance / handlePinch.prevDistance;
-//       setScale(prevScale => prevScale.multiplyScalar(scaleFactor));
-
-//       handlePinch.prevDistance = distance;
-//     }
-//   };
-
 //   return (
 //     <>
 //       <OrbitControls />
 //       <ambientLight />
 //       {isPresenting &&
 //         models.map(({ position, id }) => {
-//           return <Model key={id} gltfPath={gltfPath} position={position} scale={scale} />;
+//           return <Model key={id} gltfPath={gltfPath} position={position} />;
 //         })}
 //       {isPresenting && (
 //         <Interactive onSelect={placeModel}>
@@ -168,7 +58,7 @@ export default XrHitModelContainer;
 //         </Interactive>
 //       )}
 
-//       {!isPresenting && <Model gltfPath={gltfPath} scale={scale} />}
+//       {!isPresenting && <Model gltfPath={gltfPath} />}
 //     </>
 //   );
 // };
@@ -193,3 +83,113 @@ export default XrHitModelContainer;
 // };
 
 // export default XrHitModelContainer;
+
+
+
+import React, { useRef, useState } from 'react';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { Interactive, useHitTest, useXR } from "@react-three/xr";
+import { Canvas } from "@react-three/fiber";
+import { ARButton, XR } from "@react-three/xr";
+import { useLocation } from 'react-router-dom';
+import * as THREE from 'three';
+
+const Model = ({ gltfPath, position, scale }) => {
+  const gltf = useLoader(GLTFLoader, gltfPath);
+  return <primitive position={position} scale={scale} object={gltf.scene} />;
+};
+
+const XrHitModel = ({ gltfPath }) => {
+  const reticleRef = useRef();
+  const [models, setModels] = useState([]);
+  const [scale, setScale] = useState(new THREE.Vector3(1, 1, 1));
+  const { isPresenting } = useXR();
+
+  useThree(({ camera }) => {
+    if (!isPresenting) {
+      camera.position.z = 3;
+    }
+  });
+
+  useHitTest((hitMatrix, hit) => {
+    hitMatrix.decompose(
+      reticleRef.current.position,
+      reticleRef.current.quaternion,
+      reticleRef.current.scale
+    );
+
+    reticleRef.current.rotation.set(-Math.PI / 2, 0, 0);
+  });
+
+  const placeModel = (e) => {
+    let position = e.intersection.object.position.clone();
+    let id = Date.now();
+    setModels([{ position, id }]);
+  };
+
+  const handlePinch = (e) => {
+    if (e.touches.length === 2) {
+      const touch1 = e.touches[0];
+      const touch2 = e.touches[1];
+
+      const distance = Math.sqrt(
+        (touch1.clientX - touch2.clientX) ** 2 +
+        (touch1.clientY - touch2.clientY) ** 2
+      );
+
+      if (!handlePinch.prevDistance) {
+        handlePinch.prevDistance = distance;
+      }
+
+      const scaleFactor = distance / handlePinch.prevDistance;
+      setScale(prevScale => prevScale.multiplyScalar(scaleFactor));
+
+      handlePinch.prevDistance = distance;
+    }
+  };
+
+  return (
+    <>
+      <OrbitControls />
+      <ambientLight />
+      {isPresenting &&
+        models.map(({ position, id }) => {
+          return <Model key={id} gltfPath={gltfPath} position={position} scale={scale} />;
+        })}
+      {isPresenting && (
+        <Interactive onSelect={placeModel}>
+          <mesh ref={reticleRef} rotation-x={-Math.PI / 2}>
+            <ringGeometry args={[0.1, 0.25, 32]} />
+            <meshStandardMaterial color={"white"} />
+          </mesh>
+        </Interactive>
+      )}
+
+      {!isPresenting && <Model gltfPath={gltfPath} scale={scale} />}
+    </>
+  );
+};
+
+const XrHitModelContainer = () => {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const gltfPath = params.get('gltfPath') || './models/default.gltf';
+
+  return (
+    <>
+      <ARButton sessionInit={{
+        requiredFeatures: ["hit-test"]
+      }} />
+      <Canvas>
+        <XR>
+          <XrHitModel gltfPath={gltfPath} />
+        </XR>
+      </Canvas>
+    </>
+  );
+};
+
+export default XrHitModelContainer;
