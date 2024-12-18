0c6e3776ad300b9db3253f6641d5cbfc8a2a8d11
Rajan Dhariyaparmar <rajandalvadi2510@gmail.com>
2024-12-18 12:17:22 +0530

    bug fix of 3d view

--- client/src/components/product/MainProduct.jsx
+++ client/src/components/product/MainProduct.jsx
@@ -6,16 +6,14 @@ import { useMediaQuery } from "react-responsive";
 // import { OrbitControls } from '@react-three/drei';
 // import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 // import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
-import { Link, useParams } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useCart } from '../../CartContext'; 
-import { useNavigate } from 'react-router-dom';
-import '@google/model-viewer';
-
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useCart } from "../../CartContext";
+import { useNavigate } from "react-router-dom";
+import "@google/model-viewer";
 
 // const ProductCard = ({ gltfPath, positionY, initialScale }) => {
 //   const controlsRef = useRef();
@@ -30,7 +28,7 @@ import '@google/model-viewer';
 //     }
 
 //     console.log('GLTF Path:', gltfPath);
-    
+
 //     const loader = new GLTFLoader();
 //     const dracoLoader = new DRACOLoader();
 
@@ -62,7 +60,7 @@ import '@google/model-viewer';
 
 //   return (
 //     <div className="flex flex-col mx-[15px] my-[20px] bg-white shadow-lg max-w-[380px] max-h-[480px] rounded-[25px]">
-//         { toast.isOpen && 
+//         { toast.isOpen &&
 //          <ToastContainer
 //           position="top-right"
 //           autoClose={5000}
@@ -93,17 +91,17 @@ import '@google/model-viewer';
 // };
 
 const AddToCartButton = () => {
-  const { updateCartItems } = useCart(); 
+  const { updateCartItems } = useCart();
   const location = useLocation();
   const params = new URLSearchParams(location.search);
-  const title = params.get('title');
-  const price = params.get('price');
-  const imgsrc =params.get('imgsrc');
-  const _id =params.get('_id');
+  const title = params.get("title");
+  const price = params.get("price");
+  const imgsrc = params.get("imgsrc");
+  const _id = params.get("_id");
 
   const handleAddToCart = async () => {
-    const tokenhandle = localStorage.getItem('token');
-  
+    const tokenhandle = localStorage.getItem("token");
+
     if (tokenhandle) {
       try {
         const decoded = jwtDecode(tokenhandle);
@@ -111,13 +109,15 @@ const AddToCartButton = () => {
         const userId = decoded.id; // Get the ID of the logged-in user
 
         console.log(cartitemId);
-  
+
         // Check if the product is already in the cart for the logged-in user
-        const response = await fetch(`https://room-craft-api.vercel.app/api/cart/cartlist?userId=${userId}&cartitemId=${cartitemId}`);
+        const response = await fetch(
+          `https://room-craft-api.vercel.app/api/cart/cartlist?userId=${userId}&cartitemId=${cartitemId}`
+        );
         const data = await response.json();
-  
+
         if (data.length > 0) {
-          toast.info('You already have this product in your cart.', {
+          toast.info("You already have this product in your cart.", {
             position: "top-right",
             autoClose: 4000,
             hideProgressBar: false,
@@ -125,9 +125,9 @@ const AddToCartButton = () => {
             pauseOnHover: true,
             draggable: true,
             progress: undefined,
-            theme: "light"
+            theme: "light",
           });
-          console.log('Duplicate item in cart');
+          console.log("Duplicate item in cart");
         } else {
           // If the product is not in the cart, send a POST request to add it
           const orderData = {
@@ -137,20 +137,23 @@ const AddToCartButton = () => {
             price,
             quantity: 1,
             isorder: false,
-            cartitemId: _id
+            cartitemId: _id,
           };
-  
-          const addCartResponse = await fetch('https://room-craft-api.vercel.app/api/cart/addcart', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(orderData)
-          });
-  
+
+          const addCartResponse = await fetch(
+            "https://room-craft-api.vercel.app/api/cart/addcart",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(orderData),
+            }
+          );
+
           if (!addCartResponse.ok) {
             const errorData = await addCartResponse.json();
-            toast.error('An error occurred while adding to cart:', {
+            toast.error("An error occurred while adding to cart:", {
               position: "top-right",
               autoClose: 4000,
               hideProgressBar: false,
@@ -158,11 +161,11 @@ const AddToCartButton = () => {
               pauseOnHover: true,
               draggable: true,
               progress: undefined,
-              theme: "light"
+              theme: "light",
             });
-            console.error('Error adding item to cart:', errorData.message);
+            console.error("Error adding item to cart:", errorData.message);
           } else {
-            toast.success('Item added to cart successfully', {
+            toast.success("Item added to cart successfully", {
               position: "top-right",
               autoClose: 1000,
               hideProgressBar: true,
@@ -172,13 +175,13 @@ const AddToCartButton = () => {
               progress: undefined,
               theme: "light",
             });
-            console.log('Item added to cart successfully');
+            console.log("Item added to cart successfully");
             updateCartItems(data.newCartCount);
           }
         }
       } catch (error) {
-        console.error('Error adding item to cart:', error);
-        toast.error('An unexpected error occurred. Please try again later.', {
+        console.error("Error adding item to cart:", error);
+        toast.error("An unexpected error occurred. Please try again later.", {
           position: "top-right",
           autoClose: 4000,
           hideProgressBar: false,
@@ -186,11 +189,11 @@ const AddToCartButton = () => {
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          theme: "light"
+          theme: "light",
         });
       }
     } else {
-      toast.info('Login to add item to cart', {
+      toast.info("Login to add item to cart", {
         position: "top-right",
         autoClose: 4000,
         hideProgressBar: false,
@@ -198,14 +201,15 @@ const AddToCartButton = () => {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "light"
+        theme: "light",
       });
     }
-};
+  };
 
   return (
-    <button className="flex gap-1 px-5 w-2/12 py-1.5 mt-7 text-base font-bold text-justify text-white bg-black rounded-3xl"
-     onClick={handleAddToCart}
+    <button
+      className="flex gap-1 px-5 w-2/12 py-1.5 mt-7 text-base font-bold text-justify text-white bg-black rounded-3xl"
+      onClick={handleAddToCart}
     >
       <img
         loading="lazy"
@@ -222,9 +226,11 @@ const ProductDetails = ({ title, discription, price, gltfPath }) => {
     <section className="flex flex-col self-stretch mt-4 my-auto">
       <h1 className="text-3xl font-medium text-justify text-black">{title}</h1>
       <p className="mt-3 text-lg text-black">{discription}</p>
-      <p className="mt-4 text-3xl font-bold text-justify text-black">Rs.{price}</p>
+      <p className="mt-4 text-3xl font-bold text-justify text-black">
+        Rs.{price}
+      </p>
       <AddToCartButton />
-      
+
       <div className="">
         <Link to={`/xr?gltfPath=${gltfPath}`}>
           <button className="flex justify-center items-center px-2.5 mt-5 rounded-full bg-zinc-300 h-[60px] w-[60px]">
@@ -249,32 +255,31 @@ const MainProduct = () => {
   };
   const params = new URLSearchParams(location.search);
 
-  const gltfPath = params.get('gltfPath');
-  const title = params.get('title');
-  const discription = params.get('discription');
-  const price = params.get('price');
-  const positionY = params.get('positionY');
-  const initialScale = params.get('initialScale');
-
+  const gltfPath = params.get("gltfPath");
+  const title = params.get("title");
+  const discription = params.get("discription");
+  const price = params.get("price");
+  const positionY = params.get("positionY");
+  const initialScale = params.get("initialScale");
 
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       try {
         const decoded = jwtDecode(token);
         const _id = decoded.id;
 
         fetch(`https://room-craft-api.vercel.app/api/person/users/${_id}`)
-          .then(response => response.json())
-          .then(data => {
+          .then((response) => response.json())
+          .then((data) => {
             setUser(data.user);
             setLoading(false);
           })
-          .catch(error => {
-            console.error('Error fetching user data:', error);
+          .catch((error) => {
+            console.error("Error fetching user data:", error);
             setLoading(false);
           });
       } catch (error) {
@@ -288,7 +293,7 @@ const MainProduct = () => {
 
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   useEffect(() => {
-     {
+    {
       window.scrollTo(0, 0);
     }
   });
@@ -296,22 +301,37 @@ const MainProduct = () => {
     <>
       <Navbar />
       <div
-        className={`px-4 py-8    ${isMobile ? "mt-[15px]" : "mt-[150px]"} mb-20   bg-white rounded-3xl shadow-xl drop-shadow-2xl ${
-          isMobile ? "max-w-full" : "max-w-[898px] mx-auto"
-        }`}
+        className={`px-4 py-8    ${isMobile ? "mt-[15px]" : "mt-[150px]"
+          } mb-20   bg-white rounded-3xl shadow-xl drop-shadow-2xl ${isMobile ? "max-w-full" : "max-w-[898px] mx-auto"
+          }`}
       >
         <div className={`flex gap-5 ${isMobile ? "flex-col" : "flex-row"}`}>
-          <div className={`flex flex-col w-full ${isMobile ? "-mb-6" : "w-1/2"}`}>
-          <div className='rounded-lg' style={{ width: '480px', height: '380px' }}>
-    <model-viewer
-      src={gltfPath}
-      alt="Home Decor Product"
-      auto-rotate
-      camera-controls
-      shadow-intensity="1"
-      style={{ width: '100%', height: '100%' }}
-    ></model-viewer>
-  </div>
+          <div
+            className={`flex flex-col w-full ${isMobile ? "-mb-6" : "w-1/2"}`}
+          >
+           <div
+  className="rounded-lg"
+  style={{
+    width: "100%",
+    maxWidth: "480px",  // Maximum width for larger screens
+    height: "380px",    // Fixed height
+    margin: "0 auto",   // Centering for better alignment
+  }}
+>
+  <model-viewer
+    src={gltfPath}
+    alt="Home Decor Product"
+    auto-rotate
+    camera-controls
+    shadow-intensity="1"
+    style={{
+      width: "100%",
+      height: "100%",
+      objectFit: "contain",  // Ensure the model fits within the container
+    }}
+  ></model-viewer>
+</div>
+
           </div>
           <div className={`flex flex-col w-full ${isMobile ? "" : "w-1/2"}`}>
             <ProductDetails
